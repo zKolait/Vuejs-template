667dc41f5e2e0119941e66ba10be10531a175363
CHAPEAU Nicolas <chapenicolas@gmail.com>
2019-05-07 20:30:45 +0200

    Adding middlewares

--- src/router.js
+++ src/router.js
@@ -14,12 +14,11 @@ import auth from './middleware/auth'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
-      meta: { middleware: auth },
       component: Home
     },
     {
@@ -35,8 +34,51 @@ export default new Router({
     {
       path: '/admin',
       name: 'admin',
-      meta: { layout: 'admin', middleware: 'auth' },
+      meta: { layout: 'admin', middleware: auth },
       component: Admin
     }
   ]
 })
+
+
+// Middleware code
+//
+// ->
+function nextFactory(context, middleware, index) {
+  const subsequentMiddleware = middleware[index]
+  // If no subsequent Middleware exists,
+  // the default `next()` callback is returned.
+  if (!subsequentMiddleware) return context.next
+
+  return (...parameters) => {
+    // Run the default Vue Router `next()` callback first.
+    context.next(...parameters)
+    // Then run the subsequent Middleware with a new
+    // `nextMiddleware()` callback.
+    const nextMiddleware = nextFactory(context, middleware, index + 1)
+    subsequentMiddleware({ ...context, next: nextMiddleware })
+  }
+}
+
+// Before pushing to page if to has a middleware execute it
+router.beforeEach((to, from, next) => {
+  if (to.meta.middleware) {
+    const middleware = Array.isArray(to.meta.middleware)
+      ? to.meta.middleware
+      : [to.meta.middleware]
+
+    const context = {
+      from,
+      next,
+      router,
+      to,
+    }
+    const nextMiddleware = nextFactory(context, middleware, 1)
+
+    return middleware[0]({ ...context, next: nextMiddleware })
+  }
+
+  next()
+})
+
+export default router
